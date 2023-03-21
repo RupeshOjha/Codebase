0f809d75cb20dbb96c1e61ebb2c99cc1f8757cde
Rupesh Ojha <rupeshojha@hotmail.com>
2023-03-21 09:07:06 +0530

    initial commit

---
+++ List/Join.py
@@ -0,0 +1,15 @@
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+# example 2
+list3 = list1 + list2
+print(list3)
+
+# example 3
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
